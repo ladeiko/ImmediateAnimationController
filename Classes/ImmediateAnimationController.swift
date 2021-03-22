b00333c2ff8d491082cc9223f4a07281ee48264b
Siarhei Ladzeika <sergey.ladeiko@bpmobile.com>
2021-03-22 13:25:11 +0300

    add beginAppearanceTransition/endAppearanceTransition for source controller

--- Classes/ImmediateAnimationController.swift
+++ Classes/ImmediateAnimationController.swift
@@ -8,44 +8,58 @@
 import UIKit
 
 open class ImmediateAnimationController: NSObject, UIViewControllerAnimatedTransitioning {
-    
+
     public static let shared = ImmediateAnimationController()
-    
+
+    public var modalPresentationStylesToCallAppearanceHooks: [UIModalPresentationStyle] = [.fullScreen, .overFullScreen]
+
     public func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return 0.0
     }
-    
+
     public func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-        
+
         let fromViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.from)!
         let toViewController = transitionContext.viewController(forKey: UITransitionContextViewControllerKey.to)!
         let fromView = fromViewController.view!
         let toView = toViewController.view!
-        
+
         let containerView = transitionContext.containerView
         let isPresenting = toViewController.presentingViewController == fromViewController
-        
+
         if isPresenting {
+            if modalPresentationStylesToCallAppearanceHooks.contains(toViewController.modalPresentationStyle) {
+                fromViewController.beginAppearanceTransition(false, animated: false)
+            }
             toViewController.beginAppearanceTransition(true, animated: false)
             containerView.addSubview(toView)
         }
         else {
+            if modalPresentationStylesToCallAppearanceHooks.contains(fromViewController.modalPresentationStyle) {
+                toViewController.beginAppearanceTransition(true, animated: false)
+            }
             fromViewController.beginAppearanceTransition(false, animated: false)
             fromView.removeFromSuperview()
         }
-        
+
         let wasCancelled = transitionContext.transitionWasCancelled
         if wasCancelled {
             toView.removeFromSuperview()
         }
-        
+
         transitionContext.completeTransition(true)
-        
+
         if isPresenting {
             toViewController.endAppearanceTransition()
+            if modalPresentationStylesToCallAppearanceHooks.contains(toViewController.modalPresentationStyle) {
+                fromViewController.endAppearanceTransition()
+            }
         }
         else {
             fromViewController.endAppearanceTransition()
+            if modalPresentationStylesToCallAppearanceHooks.contains(fromViewController.modalPresentationStyle) {
+                toViewController.endAppearanceTransition()
+            }
         }
     }
 }
--- ImmediateAnimationController.podspec
+++ ImmediateAnimationController.podspec
@@ -1,13 +1,13 @@
 Pod::Spec.new do |s|
-    s.name = "ImmediateAnimationController"
-    s.version = "1.0.4"
-    s.summary = "ImmediateAnimationController implements UIViewControllerAnimatedTransitioning without any animation"
-    s.homepage = "https://github.com/ladeiko/ImmediateAnimationController"
-    s.license = { :type => "CUSTOM", :file => "LICENSE" }
-    s.author = { "Siarhei Ladzeika" => "sergey.ladeiko@gmail.com" }
-    s.platform = :ios, "9.0"
-    s.source = { :git => "https://github.com/ladeiko/ImmediateAnimationController.git", :tag => "#{s.version}" }
-    s.source_files = "Classes/**/*.{m,s,swift}"
-    s.frameworks = "UIKit"
-    s.requires_arc = true
+  s.name = "ImmediateAnimationController"
+  s.version = "1.1.0"
+  s.summary = "ImmediateAnimationController implements UIViewControllerAnimatedTransitioning without any animation"
+  s.homepage = "https://github.com/ladeiko/ImmediateAnimationController"
+  s.license = { :type => "CUSTOM", :file => "LICENSE" }
+  s.author = { "Siarhei Ladzeika" => "sergey.ladeiko@gmail.com" }
+  s.platform = :ios, "9.0"
+  s.source = { :git => "https://github.com/ladeiko/ImmediateAnimationController.git", :tag => "#{s.version}" }
+  s.source_files = "Classes/**/*.{m,s,swift}"
+  s.frameworks = "UIKit"
+  s.requires_arc = true
 end
